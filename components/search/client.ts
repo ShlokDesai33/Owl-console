78d0896f83699a7de18e45b971e529e42acaa2dc
shlokdesai <shlokdesai33@gmail.com>
2022-12-04 16:54:48 -0600

    search api key fix

--- components/search/client.ts
+++ components/search/client.ts
@@ -2,7 +2,7 @@ import algoliasearch from 'algoliasearch/lite'
 
 const algoliaClient = algoliasearch(
   '0Q9AHOYTD6',
-  'e5939ceebf94733c18e54c72a3d2686e' // owl-console.vercel.app restricted search key
+  '4fbd4061c030a50adcb8acfacc323cd3' // owl-console.vercel.app restricted search key
 );
 
 export default algoliaClient;
